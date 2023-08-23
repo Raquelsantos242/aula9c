9245a17f09d5c05d4f8d14f4d13164cfe981ac96
Raquel Braga <87723504+Raquelsantos242@users.noreply.github.com>
2023-08-23 09:07:17 -0300

    Add files via upload

---
+++ Biblioteca.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace etapa5Aula1
+{
+    public abstract class Veiculo
+    {
+
+        private string _tipoCombustivel;
+
+        public Veiculo()
+        {
+
+        }
+
+
+        public Veiculo(string tipoCombustivel)
+        {
+            _tipoCombustivel = tipoCombustivel;
+        }
+
+        public abstract void Mover();
+
+        public virtual void Abastecer()
+        {
+            Console.WriteLine("Abastecimento padrão");
+        }
+
+    }
+
+    internal class Carro: Veiculo
+    {
+
+    public Carro(string tipoCombustivel):base(tipoCombustivel)
+        {
+
+        }
+        public override void Mover()
+        {
+            Console.WriteLine("Ir para trás e ir para frente....");
+        }
+
+        public override void Abastecer()
+        {
+            Console.WriteLine("Carro Elétrico");
+        }
+    }
+
+    internal class Aviao: Veiculo
+    {
+        public Aviao(string tipoCombustivel) : base(tipoCombustivel)
+        {
+
+        }
+        public override void Mover()
+        {
+            Console.WriteLine("Avião só vai pra frente....");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using etapa5Aula1;
+
+class TestClass {
+
+    static void Main(String[] args)
+    {
+
+
+        //Veiculo v = new Veiculo(); //Classe abstrata não pode ser instanciada
+
+        // Console.WriteLine(v.TipoCombustivel);
+
+        Carro c = new Carro("gasolina");
+        Aviao v = new Aviao("gasolina");
+        
+        Console.ReadKey();
+    }
+
+
+
+}
+
